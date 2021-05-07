c638c283bb2fe1411acacdac5312e4ded2876f3c
Andy <andy@Andys-MacBook-Pro.local>
2021-05-07 01:35:29 -0400

    test component

---
+++ MyComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <p>{{ label }}: {{ count }}</p>
+    <button @click="add">Add</button>
+    <p>(the next value is {{ next }})</p>
+    <p>
+      Label: <input type="text" v-model="label" />
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    interval: {
+      type: Number,
+      default: 1
+    }
+  },
+  data() {
+    return {
+      count: 0,
+      label: 'Count'
+    }
+  },
+  computed: {
+    next() {
+      return this.count + this.interval
+    }
+  },
+  methods: {
+    add() {
+      this.count++
+    }
+  }
+}
+</script>
