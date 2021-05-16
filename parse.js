9efdbfde46d669332d0654cbf693f2e202f80dde
Andy <andy@Andys-MacBook-Pro.local>
2021-05-16 16:20:12 -0400

    prevent()

--- parse.js
+++ parse.js
@@ -1,11 +1,40 @@
 const Parser = require('acorn-loose');
 const fs = require('fs')
 
-const component = fs.readFileSync('./MyComponent.vue', 'utf8')
-console.log(component.split('\n').length)
-let script = component.split(/<script.*\>/)[1].split('</script>')[0]
+function prevent(key, cond) {
+  if(!cond) { return }
+  const messages = {
+    'NO_CONTENT': 'no content in the file',
+    'NO_SCRIPT': 'no <script> found',
+    'NO_CODE': 'no code in <script>',
+    'NO_EXPORT': 'need export default',
+    'NO_OBJECT': 'need to export an object literal',
+  }
+  const message = messages[key]
+  console.log('parser error: ' + message)
+  throw new Error()
+}
 
-const options = {}
+const file = fs.readFileSync('./MyComponent.vue', 'utf8')
+const splits = file.split(/<script.*\>/)
 
-const ast = Parser.parse(script, options)
-console.log(ast.body[0].declaration.properties)
+prevent('NO_SCRIPT', splits.length === 1)
+
+let script = splits[1].split('</script>')[0].trim()
+
+prevent('NO_CODE', script === '')
+
+const ast = Parser.parse(script, {})
+const lastNode = ast.body[ast.body.length-1]
+
+prevent('NO_EXPORT', lastNode.type !== 'ExportDefaultDeclaration')
+
+const declaration = lastNode.declaration
+
+prevent('NO_OBJECT', declaration.type !== 'ObjectExpression')
+
+const properties = declaration.properties
+
+properties.forEach(op => {
+  console.log(op.key)
+})
